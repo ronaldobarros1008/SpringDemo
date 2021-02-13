04bb3ce21a374035d1dbb60d5e96b08717e7646b
ronaldo.barros <ronaldo.barros@iesb.br>
2021-02-13 13:21:38 -0300

    first commit

---
+++ src/main/java/com/example/demo/resources/HomeResource.java
@@ -0,0 +1,16 @@
+package com.example.demo.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/")
+public class HomeResource {
+	
+	@GetMapping
+	public String getHello() {
+		return "Opa, to no ar, safados!!!";
+	}
+
+}
